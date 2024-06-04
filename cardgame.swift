5592f1560822adf9d76592dbf2740a8f0b303912
Chaitanya <puranikc@shernet.sheridancollege.ca>
2024-06-03 20:16:12 -0400

    Implemented method playRound()

--- cardgame.swift
+++ cardgame.swift
@@ -9,4 +9,34 @@ class CardGame {
         self.deck = Deck()
         self.deck.shuffle()
     }
+
+    func playRound(playerChoice: Int) -> (winner: String, points: Int)? {
+    let playerCardOptional = deck.drawCard(position: playerChoice)
+    if playerCardOptional == nil {
+        return nil
+    }
+    let playerCard = playerCardOptional!
+    
+    let systemCardOptional = deck.drawRandomCard()
+    if systemCardOptional == nil {
+        return nil
+    }
+    let systemCard = systemCardOptional!
+    
+    let playerCardValue = playerCard.value
+    let systemCardValue = systemCard.value
+    
+    if playerCardValue > systemCardValue {
+        let pointsScored = playerCardValue - systemCardValue
+        playerPoints += pointsScored
+        return (playerName, pointsScored)
+    } else if systemCardValue > playerCardValue {
+        let pointsScored = systemCardValue - playerCardValue
+        systemPoints += pointsScored
+        return ("System", pointsScored)
+    } else {
+        return ("Tie", 0)
+    }
+}
+
 }
